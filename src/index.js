28e501b34fc0eecaac2c625b1c9c966db1b4a026
marcos-asdes <marco.engc@poli.ufrj.br>
2022-03-25 16:21:24 -0300

    added style and reset CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
+import "../src/assets/styles/reset.css";
+import "../src/assets/styles/style.css";
 
 ReactDOM.render(<App />, document.querySelector(".root"));
